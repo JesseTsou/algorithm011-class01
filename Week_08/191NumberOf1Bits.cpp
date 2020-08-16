4933ae2a3b6eb7723999da01ec6d10cf90050cb5
zouyajie <zou_yajie@venustech.com.cn>
2020-08-17 02:04:13 +0800

    【homework】add week8

---
+++ Week_08/191NumberOf1Bits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int count = 0;
+        int mask = 1;
+        for (int i = 0; i < 32; i ++){
+            if (n & 1){
+                count ++;
+            }
+            n /= 2;
+        }
+        return count;
+    }
+};
